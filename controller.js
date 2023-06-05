89a3418ba49b4696b0397f7d41282aaa32998237
23mervinwijaya <23mervinwijaya@gmail.com>
2023-06-06 01:37:34 +0700

    melengkapi function

--- controller.js
+++ controller.js
@@ -1,49 +1,46 @@
-function getPilhanCom (){
+function getPilihanCom(){
     const pilihan = Math.random();
-    if(pilihan<=0.33) return 'batu';
+    if(pilihan<=0.33) return "batu";
     if(pilihan>=0.34 && pilihan <=0.66) return "gunting";
-    if(pilhan>=0.67) return "kertas";
-
+    if(pilihan>=0.67) return "kertas";
 }
 
 function getHasil(pilihanPlayer,pilihanCom){
-    if(pilihanPlayer==pilihanCom) return "Seri";
-    if(pilihanPlayer=='batu') return (pilihanCom=='gunting')?'Player Menang':'Com Menang';
-    if(pilihanPlayer=='gunting') return (pilihanCom=='kertas')?'Player Menang':'Com Menang';
-    if(pilihanPlayer=='kertas') return (pilhanCom=='batu')?'Player Menang':'Com Menang'
+    if(pilihanPlayer==pilihanCom) return "seri";
+    if(pilihanPlayer=="batu") return (pilihanCom=="gunting")?"Player WON" : "Com WON";
+    if(pilihanPlayer=="gunting") return (pilihanCom=="kertas")?"player WON" : "Com WON";
+    if(pilihanPlayer=="kertas") return (pilihanCom=="batu")?"Player WON" : "Com WON";
 }
 
-const pBatu = document.querySelector('.batu')
-const pGunting = document.querySelector('.gunting')
-const pKertas = document.querySelector('.kertas')
+const pilihan = document.querySelectorAll('.player img');
+pilihan.forEach(function(pil){
+    pil.addEventListener('click',function(){
+        pil.style.backgroundColor = 'rgba(173, 173, 173, 0.541)';
+        pil.style.borderRadius = '25px';
+        
+        const pilihanPlayer = pil.className;
+        const pilihanCom = getPilihanCom();
+        const hasil = getHasil(pilihanPlayer,pilihanCom);
+        
+        const imgComp = document.querySelectorAll('.computer img');
+        imgComp.forEach(function(img){
+            const className = img.className;
 
+            if(className == pilihanCom){
+                img.style.backgroundColor = 'rgba(173, 173, 173, 0.541)';
+                img.style.borderRadius = '25px';
+            }
+        })
 
-// Listener Pilihan Batu
-pBatu.addEventListener('click',function(){
-    const pilihanCom = getPilhanCom();
-    const pilihanPlayer = pBatu.className;
-    const hasil = getHasil(pilihanCom,pilihanPlayer);
+        const result = document.querySelector('.result');
+        result.innerHTML = hasil;
 
-    const result = document.querySelector('.result');
-    result.innerHTML = hasil;
-})
-
-// Listener Pilihan Gunting
-pGunting.addEventListener('click',function(){
-    const pilihanCom = getPilhanCom();
-    const pilihanPlayer = pGunting.className;
-    const hasil = getHasil(pilihanCom,pilihanPlayer);
-
-    const result = document.querySelector('.result');
-    result.innerHTML = hasil;
-})
-
-// Listener Pilihan Kertas
-pKertas.addEventListener('click',function(){
-    const pilihanCom = getPilhanCom();
-    const pilihanPlayer = pKertas.className;
-    const hasil = getHasil(pilihanCom,pilihanPlayer);
-
-    const result = document.querySelector('.result');
-    result.innerHTML = hasil;
+        function reset(){
+            setTimeout(()=>{
+                alert("Ingin main lagi?")
+                window.location.href = "/game.html"
+            },500)
+        }
+        reset()
+    })
 })
